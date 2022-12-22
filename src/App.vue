37a46070f35493f6399aea5395870a712baac9a1
7ldsma <alvaro.ledesmav@gmail.com>
2022-12-22 16:31:05 +0100

    bonus

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,20 @@
       <navbar/>
     </div>
 
-    <div class="px-8 md:px-12 md:mx-28 h-80">
+    <div class="flex flex-col px-8 md:px-12 md:mx-28 h-80">
       {{ sumatoria }}
-      <div v-if="hola">
-        <mensaje/>
+      <button class="bg-[#c9c19f] px-6 py-6" @click="mostrarMensaje">Mostrar mensaje</button>
+      <button class="bg-[#c9c19f] px-6 py-6" @click="ocultarMensaje">ocultar mensaje</button>
+      <div v-if="this.hola">
+        Este mensaje se muestra por el valor true de una variable.
+      </div>
+
+      <div>
+        <button class="bg-gray-300 py-2 px-4 rounded-md" @click="cambiarBg">Cambiar color</button>
+        <ul :class="{background: bright}">
+          <li v-for="post in posts">{{ post.Title }}</li>
+        </ul>
+
       </div>
 
     </div>
@@ -22,18 +32,39 @@
 <script>
 import navbar from './components/navbar.vue'
 import footercomp from './components/footercomp.vue'
-import mensaje from './components/mensaje.vue'
 
 export default {
   components: {
     navbar,
     footercomp,
-    mensaje
+    
   },
 
   data(){
     return {
       suma : 2 + 2,
+      hola: false,
+      bright: false,
+      posts: [{
+          Title: 'Uno',
+          Description:'Number1',
+          content:'this is a number',
+          
+          }, 
+          {
+          Title: 'Dos',
+          Description:'Number2',
+          content:'this is the second number',
+          
+          },
+          {
+          Title: 'Tres',
+          Description:'Number3',
+          content:'this is not a number',
+        
+          }
+
+      ]
     }
   },
 
@@ -42,10 +73,27 @@ export default {
       return this.suma
     }
 
+  },
+  methods: {
+    mostrarMensaje (){
+      this.hola = true
+    },
+    ocultarMensaje(){
+      this.hola = false
+    },
+    cambiarBg (){
+      this.bright = true
+    }
+
   }
 
 };
 </script>
 
 <style scoped>
+
+.background {
+  background-color: azure;
+}
+
 </style>
